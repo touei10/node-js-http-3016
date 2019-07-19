d9195b0b044b0dc31d0419b1c3f5a443724f9db4
touei10 <52106355+touei10@users.noreply.github.com>
2019-07-20 03:51:12 +0900

    Herokuで起動できるように変更

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict';
 const http = require('http');
-const jade = require('jade');
+const pug = require('pug');
 const server = http.createServer((req, res) => {
   const now = new Date();
   console.info('[' + now + '] Requested by ' + req.connection.remoteAddress);
@@ -11,19 +11,19 @@ const server = http.createServer((req, res) => {
   switch (req.method) {
     case 'GET':
       if (req.url === '/enquetes/yaki-shabu') {
-        res.write(jade.renderFile('./form.jade', {
+        res.write(pug.renderFile('./form.pug', {
           path: req.url,
           firstItem: '焼き肉',
           secondItem: 'しゃぶしゃぶ'
         }));
       } else if (req.url === '/enquetes/rice-bread') {
-        res.write(jade.renderFile('./form.jade', {
+        res.write(pug.renderFile('./form.pug', {
           path: req.url,
           firstItem: 'ごはん',
           secondItem: 'パン'
         }));
       } else if (req.url === '/enquetes/sushi-pizza') {
-        res.write(jade.renderFile('./form.jade', {
+        res.write(pug.renderFile('./form.pug', {
           path: req.url,
           firstItem: '寿司',
           secondItem: 'ピザ'
@@ -39,7 +39,7 @@ const server = http.createServer((req, res) => {
         body = Buffer.concat(body).toString();
         const decoded = decodeURIComponent(body);
         console.info('[' + now + '] 投稿: ' + decoded);
-        res.write('<!DOCTYPE html><html lang="jp"><head><meta charset="utf-8"></head><body><h1>' +
+        res.write('<!DOCTYPE html><html lang="ja"><head><meta charset="utf-8"></head><body><h1>' +
           decoded + 'が投稿されました</h1></body></html>');
         res.end();
       });
@@ -53,7 +53,7 @@ const server = http.createServer((req, res) => {
 }).on('clientError', (e) => {
   console.error('[' + new Date() + '] Client Error', e);
 });
-const port = 8000;
+const port = process.env.PORT || 8000;
 server.listen(port, () => {
-  console.info('[' + new Date() + '] Listening on ' + port);
+  console.info('  =>  Server running!\n[' + new Date() + '] Listening on ' + port);
 });
