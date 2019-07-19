cd9b7faecff68194905e49fb2b1f9a0a34f0dc1d
touei10 <52106355+touei10@users.noreply.github.com>
2019-07-20 04:15:27 +0900

    Herokuで起動できるように変更

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ const server = http.createServer((req, res) => {
 }).on('clientError', (e) => {
     console.error('[' + new Date() + '] Client Error', e);
 });
-const port = 8000;
+const port = process.env.PORT || 8000;
 server.listen(port, () => {
-    console.info('[' + new Date() + '] Listening on ' + port);
+    console.info('  =>  Server running!\n' + '[' + new Date() + '] Listening on ' + port);
 });
